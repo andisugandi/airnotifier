400ef44386df5df8a0cb11c752a384667461b36a
Georg Glas <georg@glas.eu.org>
2024-04-03 13:21:22 +0200

    Airnotifier should log to stdout/stderr for docker logs to work #8
    https://github.com/BiP-org/airnotifier/issues/8

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-export LOGDIR=/var/log/airnotifier
-export LOGFILE=$LOGDIR/airnotifier.log
-export LOGFILE_ERR=$LOGDIR/airnotifier.err
+export LOGDIR="${LOGDIR:/var/log/airnotifier}"
+export LOGFILE="${LOGFILE:airnotifier.log}"
+export LOGFILE_ERR="${LOGFILE_ERR:airnotifier.err}"
 
 if [ ! -f "./config.py" ]; then
   cp config.py-sample config.py
@@ -15,17 +15,23 @@ if [ ! -f "./logging.ini" ]; then
   cp logging.ini-sample logging.ini
 fi
 
-sed -i "s/mongouri = \"mongodb:\/\/localhost:27017\/\"/mongouri = \"mongodb:\/\/${MONGO_SERVER-localhost}:${MONGO_PORT-27017}\"/g" ./config.py
-
-if [ ! -f "$LOGFILE" ]; then
-  touch "$LOGFILE"
+if [ ! -f "${LOGDIR}/${LOGFILE}" ]; then
+   ln -sf /dev/stdout "${LOGDIR}/${LOGFILE}"
 fi
 
-if [ ! -f "$LOGFILE_ERR" ]; then
-  touch "$LOGFILE_ERR"
+if [ ! -f "${LOGDIR}/${LOGFILE_ERR}" ]; then
+   ln -sf /dev/stderr "${LOGDIR}/${LOGFILE_ERR}"
 fi
+
+
+sed -i "s/mongouri = \"mongodb:\/\/localhost:27017\/\"/mongouri = \"mongodb:\/\/${MONGO_SERVER-localhost}:${MONGO_PORT-27017}\/\"/g" ./config.py
+sed -i "s/mongohost = \"localhost\"/mongohost = \"mongodb\"/g"  ./config.py
 
 echo "Installing AirNotifier ..."
 pipenv run ./install.py
+
 echo "Starting AirNotifier ..."
-pipenv run ./app.py >> "$LOGFILE" 2>> "$LOGFILE_ERR"
+__starttimestamp=`date +%s.%N`
+pipenv run ./app.py 
+__runtime=$( echo "${__endtimestamp} - ${__starttimestamp}" | bc -l )
+echo ${__runtime}
